1c29781bb6e1982cf4d93d0c00e76922ac0ccf86
LarsAndre <lars.andre.h@gmail.com>
2025-07-07 21:12:53 +0200

    Rebuilt the project
    Removed the emulator and reorganized some files

--- RustChip8Emulator/target/debug/deps/chip8-89d867ef3eacb5a7.d
+++
@@ -1,5 +0,0 @@
-C:\Users\cyflarsah\Documents\Programming-Projects\RustChip8Emulator\target\debug\deps\chip8-89d867ef3eacb5a7.d: chip8\src\main.rs
-
-C:\Users\cyflarsah\Documents\Programming-Projects\RustChip8Emulator\target\debug\deps\libchip8-89d867ef3eacb5a7.rmeta: chip8\src\main.rs
-
-chip8\src\main.rs:
--- RustChip8Emulator/target/debug/deps/chip8-c4cfab5d39eab528.d
+++
@@ -1,5 +0,0 @@
-C:\Users\cyflarsah\Documents\Programming-Projects\RustChip8Emulator\target\debug\deps\chip8-c4cfab5d39eab528.d: chip8\src\main.rs
-
-C:\Users\cyflarsah\Documents\Programming-Projects\RustChip8Emulator\target\debug\deps\libchip8-c4cfab5d39eab528.rmeta: chip8\src\main.rs
-
-chip8\src\main.rs:
